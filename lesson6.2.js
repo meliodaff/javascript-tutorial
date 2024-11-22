98918e8da3dfbc59e443bfc0f4f82a753b23d528
helpmecodepls1 <bialen.jv.distor@gmail.com>
2024-11-22 11:26:28 +0800

    6a - 6c done

--- lesson6.2.js
+++ lesson6.2.js
@@ -1,1 +1,20 @@
+let hour = '5';
+let name = 'Jay'
 
+if (hour >= 13 && hour <= 17) {
+    console.log(`Good afternoon, ${name}`);
+}
+else if (hour >= 6 && hour <= 12) {
+    console.log(`Good morning, ${name}`);
+}
+else{
+    console.log(`Goodnight, ${name}`);
+}
+
+console.log(add(4,5));
+
+function add(x, y){
+    let sum = x + y;
+    console.log(sum);
+    return sum;
+}
