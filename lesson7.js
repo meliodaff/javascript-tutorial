b3131b62d85d706d40cb523060f0c8584a7bdb0a
Jv Bialen <bialen.jv.distor@gmail.com>
2024-11-24 16:32:21 +0800

    done challenge exercises

---
+++ lesson7.js
@@ -0,0 +1,50 @@
+function greet(name) {
+  !name ?  console.log(`Hi there`) : console.log(`Hello, ${name}`);
+  
+}
+
+function convertToFahrenheit(celcius) {
+  let fahrenheit = ((celcius * 9 / 5) + 32) + 'F';
+  return fahrenheit;
+}
+
+function convertToCelcius(fahrenheit) {
+  let celcius = ((fahrenheit - 32) * 5 / 9) + 'C';
+  return celcius
+}
+
+function convertTemperature(degrees, unit) {
+  if (unit == 'C'){
+    return convertToFahrenheit(degrees);
+  }
+  else if (unit == 'F'){
+    return convertToCelcius(degrees);
+  }
+}
+
+function convertLength(length, from, to) {
+  if (from === 'miles' && to === 'km'){
+    let km = length * 1.6 + ' km';
+    return km;
+  } 
+  else if (from === 'km' && to === 'km'){
+      return length;
+    }
+  else if (from === 'km' && to === 'miles'){
+    let mile = length / 1.6 + ' miles';
+    return mile;
+  }
+  else if (from === 'miles' && to === 'ft'){
+    let ft = length * 5280 + ' ft';
+    return ft;
+  }
+  else if (from === 'km' && to === 'ft'){
+    let ft = length * 3281 + ' ft';
+    return ft;
+  }
+  else {
+    return `Invalid unit : ${from} to ${to}`;
+  }
+}
+
+console.log(convertLength(5, 'km', 'lbs'));
