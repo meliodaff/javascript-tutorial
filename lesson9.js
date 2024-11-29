e4ebbba0e6c9dc361434a1bf1db3d824951dfe89
helpmecodepls1 <bialen.jv.distor@gmail.com>
2024-11-29 10:30:44 +0800

    validating customer's price

---
+++ lesson9.js
@@ -0,0 +1,30 @@
+/*
+function subscribe(){
+    let btnSubscribe = document.getElementsByClassName('btn')[0];
+    
+    btnSubscribe.innerHTML = btnSubscribe.innerHTML === 'Subscribe' ? 'Subscribed' : 'Subscribe'
+}
+
+function calculate(){
+    let total = 0;
+    let userInput = document.getElementsByClassName('userInput')[0].value;
+
+    if (userInput == ''){
+        alert('No inputted value');
+        return;
+    }
+    else if (userInput == '0'){
+        alert('watdafuck')
+        return;
+    }
+    else if(userInput < 40){
+       total = +userInput + 10;
+    }
+    else{
+        total = userInput;
+    }
+
+    document.getElementsByClassName('total')[0].innerHTML = `Total: $${total}`;
+}
+    */
+
