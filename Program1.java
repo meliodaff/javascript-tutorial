97f064ab1d3d0515d5008c8681c0236967e145de
helpmecodepls1 <bialen.jv.distor@gmail.com>
2024-11-29 10:31:10 +0800

    trying to code in vs code

---
+++ Program1.java
@@ -0,0 +1,8 @@
+public class Program1 {
+    public static void main(String[] args) {
+        String subjectA = new String ("Programming");
+        String subjectB = "Programming";
+
+        System.out.println(subjectA == subjectB);
+    }
+}
