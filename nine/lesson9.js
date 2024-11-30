a5ca98ec281520565f0672da43c505ae466dfdba
Jv Bialen <bialen.jv.distor@gmail.com>
2024-11-30 18:41:46 +0800

    already did the challenges these past days

--- nine/lesson9.js
+++ nine/lesson9.js
@@ -66,3 +66,12 @@ function enter(){
         submit();
     }
 }
+
+let text = 'Hello, world'; 
+let myArray = text.split('');
+myArray.push('jay');
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+    
+}
+console.log(myArray.length);
