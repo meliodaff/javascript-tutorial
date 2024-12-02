6301744133a4c5f95d6fb6bfd50a0bbbb13cbcad
helpmecodepls1 <bialen.jv.distor@gmail.com>
2024-12-02 23:03:41 +0800

    added classes and objects

---
+++ javaFiles/manipulatingStrings.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class manipulatingStrings{
+    public static void main(String[] args){
+        String sentence = "The quick brown fox jumps over the lazy dog";
+        String[] words = sentence.split(" ");
+
+        System.out.println(words);
+
+
+        
+    }
+}
