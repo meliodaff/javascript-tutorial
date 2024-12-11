c0b84b1c36055ed2bc7332bc3dd6ce002e2451cd
helpmecodepls1 <bialen.jv.distor@gmail.com>
2024-12-11 12:02:06 +0800

    changed int to byte

--- javaFiles/CountEvenAndOdd.java
+++ javaFiles/CountEvenAndOdd.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class CountEvenAndOdd{
     public static void main(String[] args) {
         
-        int array[] = {10, 20, 30, 15, 25};
+        byte array[] = {10, 20, 30, 15, 25};
         System.out.println(count(array));
 
     }
-    public static String count(int array[]){
-        int even = 0;
-        int odd = 0;
-        for (int i = 0; i < array.length; i++) {
+    public static String count(byte array[]){
+        byte even = 0;
+        byte odd = 0;
+        for (byte i = 0; i < array.length; i++) {
                 if(array[i] % 2 == 0){
                     even++;
                 }
