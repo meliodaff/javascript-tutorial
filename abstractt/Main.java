f22b714ad5db97a7d1db60f22612125983983f6d
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2024-12-12 20:29:22 +0800

    tried the static member and called it using the class name

--- abstractt/Main.java
+++ abstractt/Main.java
@@ -2,6 +2,10 @@ package abstractt;
 
 public class Main {
     public static void main(String[] args) {
+    Student student1 = new Student();
+    Student student2 = new Student();
+
+
     System.out.println(Student.numberOfStudents);
     }
 }
