23b073547fa0691721c709d12a5446b275ec67b6
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2024-12-13 09:53:07 +0800

    lesson12
    12g to 12i arent done yet

---
+++ twelve/lesson12.1.js
@@ -0,0 +1,36 @@
+//12.A
+const add = () => {
+    console.log(2 + 3);
+}
+
+add();
+add();
+
+//12.VB
+const runTwice = (fun) => {
+    console.log(fun);
+    console.log(fun);
+}
+
+runTwice('12b');
+
+//12.C
+function start(){
+    const button = document.getElementsByClassName('test-button')[0]
+    button.innerText = 'Loading...'
+    setTimeout(function () {
+        button.innerText = 'Finished!'
+    }, 1000)
+}
+
+//12.E
+let timeoutID;
+function addToCart(){
+    const demo = document.getElementsByClassName('demo')[0]
+    demo.innerHTML = 'Added';
+    clearTimeout(timeoutID)
+    timeoutID = setTimeout(function() {
+        demo.innerHTML = '';
+    }, 2000)
+}
+
