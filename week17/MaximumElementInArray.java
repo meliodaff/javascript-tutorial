1b83e704f98ccd533011a1f1cf279b6c8927d5d1
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2024-12-13 14:34:20 +0800

    MaximumElementInArray

---
+++ week17/MaximumElementInArray.java
@@ -0,0 +1,25 @@
+package week17;
+
+
+public class MaximumElementInArray{
+    public static void main(String[] args) {
+        
+        byte[] array = {5, 8, 3, 12, 6};
+        byte[] array1 = {-10, -3, -7, -1};
+
+        System.out.println(findMaxInArray(array));
+        System.out.println(findMaxInArray(array1));
+
+    }
+    public static String findMaxInArray(byte[] array){
+
+        byte temp = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(temp < array[i]){
+                temp = array[i];
+            }
+        }
+
+        return "Maximum element = : " + temp;
+    }
+}
